e8ac242c82e533a9e3ced3511b2735d69bb0bc59
shyqulreyad <shyqulislam@gmail.com>
2023-03-29 12:47:22 +0600

    creating node

--- linklist.py
+++ linklist.py
@@ -4,3 +4,11 @@ class _Node:
     def __init__(self, element, next):
         self._element = element
         self._next = next
+        
+n1 = _Node(7, None)
+print(n1)
+print(n1._next)
+n2 = _Node(8, None)
+n1._next = n2
+print(n1._next)
+# print(n2._next)
