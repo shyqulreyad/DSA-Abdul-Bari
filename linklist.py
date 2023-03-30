ca62c2d330a8b15f572f0d36d5f9eb2151518606
shyqulreyad <shyqulislam@gmail.com>
2023-03-30 11:32:59 +0600

    head and tail

--- linklist.py
+++ linklist.py
@@ -6,9 +6,13 @@ class _Node:
         self._next = next
         
 n1 = _Node(7, None)
-print(n1)
-print(n1._next)
+# print(n1)
+# print(n1._next)
 n2 = _Node(8, None)
 n1._next = n2
-print(n1._next)
-print(n2._next)
+head = n1
+y = head
+y= head._next
+print(head, tail)
+# print(n1._next)
+# print(n2._next)
